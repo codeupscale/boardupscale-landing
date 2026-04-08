620ab4ae8d90cd50ed7a81d3acda207a5994a645
Adnan Mumtaz <adnanmumtazmayo@gmail.com>
2026-04-08 06:36:08 +0500

    feat: add Google Analytics gtag

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
+import Script from 'next/script'
 import './globals.css'
 
 const inter = Inter({ subsets: ['latin'] })
@@ -56,6 +57,18 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
+      <Script
+        src="https://www.googletagmanager.com/gtag/js?id=G-BRV3TRQYSG"
+        strategy="afterInteractive"
+      />
+      <Script id="gtag-init" strategy="afterInteractive">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', 'G-BRV3TRQYSG');
+        `}
+      </Script>
       <head>
         <script
           type="application/ld+json"
